60cf77a9e299f42c3356bff35da15e7c29bb3af1
pgm-kilidebo <kilian.debock@student.arteveldehs.be>
2022-04-21 15:17:53 +0200

    22.16.4.2: Refactor; Colleges should be in Colleges

--- Exercises/demo_02/src/app/components/ClockWithClass.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class ClockWithClass extends React.Component {
-  render() {
-    return <div className="test">{new Date().toLocaleTimeString()}</div>;
-  }
-}
-
-export default ClockWithClass;
